8f2fd001b2863396b5839dba56fb49aaeec926a6
msoler75 <msoler75@yahoo.es>
2023-06-23 22:26:29 -0400

    mejora en evento

--- resources/js/Pages/Eventos/Evento.vue
+++ resources/js/Pages/Eventos/Evento.vue
@@ -1,37 +1,49 @@
 <template>
-    <div class="max-w-[960px] pt-7 mx-auto space-y-12 mt-12">
-        <h1>{{ evento.titulo }} {{ evento.titulo }}</h1>
-        <div class="flex gap-12 flex-wrap">
-      <div class="max-w-[500px]">
-        <img :src="evento.imagen" alt="Imagen del evento" class="w-full mb-4">
-        <p class="mb-4">{{ evento.texto }}</p>
-      </div>
-      <div>
-        <div class="card bg-base-100 shadow p-4">
-          <p class="mb-2 flex gap-3 items-center">
-            <Icon icon="ph:calendar-check-duotone" class="text-xl"/> Inicio: {{ evento.fecha_inicio }}</p>
-          <p v-if="evento.fecha_fin" class="mb-2 flex gap-3 items-center"><Icon icon="ph:calendar-x-duotone" class="text-xl"/>  Fin: {{ evento.fecha_fin }}</p>
-          <p v-if="evento.hora_inicio" class="mb-2 flex gap-3"><Icon icon="ph:alarm-duotone" class="text-xl"/>  Hora de inicio: {{ evento.hora_inicio }}</p>
-          <p v-if="evento.hora_fin" class="mb-2"><Icon icon="ph:clock-countdown-duotone" class="text-xl"/>  Hora de fin: {{ evento.hora_fin }}</p>
-          <p v-if="evento.lugar" class="mb-2 flex gap-3"><Icon icon="ph:map-pin-duotone" class="text-xl"/> Lugar: {{ evento.lugar }}</p>
+    <div class="container py-12 mx-auto space-y-12 mt-12">
+        <h1 class="px-2 sm:px-5 sticky top-5 bg-base-100 py-5 border rounded-lg shadow border-base-300 z-30">{{
+            evento.titulo }} {{ evento.titulo }}</h1>
+        <div class="container mx-auto flex flex-wrap md:flex-nowrap gap-7 px-2 sm:px-5">
+            <div class="w-full md:w-1/2 md:order-last">
+                <div class="card bg-base-100 md:max-w-[300px] shadow p-4">
+                    <p class="mb-2 flex gap-3 items-center">
+                        <Icon icon="ph:calendar-check-duotone" class="text-xl" /> Inicio: {{ evento.fecha_inicio }}
+                    </p>
+                    <p v-if="evento.fecha_fin" class="mb-2 flex gap-3 items-center">
+                        <Icon icon="ph:calendar-x-duotone" class="text-xl" /> Fin: {{ evento.fecha_fin }}
+                    </p>
+                    <p v-if="evento.hora_inicio" class="mb-2 flex gap-3">
+                        <Icon icon="ph:alarm-duotone" class="text-xl" /> Hora de inicio: {{ evento.hora_inicio }}
+                    </p>
+                    <p v-if="evento.hora_fin" class="mb-2">
+                        <Icon icon="ph:clock-countdown-duotone" class="text-xl" /> Hora de fin: {{ evento.hora_fin }}
+                    </p>
+                    <p v-if="evento.lugar" class="mb-2 flex gap-3">
+                        <Icon icon="ph:map-pin-duotone" class="text-xl" /> Lugar: {{ evento.lugar }}
+                    </p>
+                </div>
+                <hr class="my-7" />
+                <p class="mt-4">{{ evento.descripcion }}</p>
+            </div>
+            <div class="w-full md:w-1/2">
+                <div class="lg:max-w-[500px]">
+                    <img :src="evento.imagen" alt="Imagen del evento" class="w-full mb-4">
+                    <p class="mb-4">{{ evento.texto }}</p>
+                </div>
+            </div>
         </div>
-        <hr class="my-7"/>
-        <p class="mt-4">{{ evento.descripcion }}</p>
-      </div>
     </div>
-    </div>
-  </template>
+</template>
 
-  <script setup>
-  import AppLayout from '@/Layouts/AppLayout.vue'
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue'
 
-  defineOptions({ layout: AppLayout })
+defineOptions({ layout: AppLayout })
 
-  const props = defineProps({
+const props = defineProps({
     evento: {
-      type: Object,
-      required: true,
+        type: Object,
+        required: true,
     },
-  })
+})
 
-  </script>
+</script>
