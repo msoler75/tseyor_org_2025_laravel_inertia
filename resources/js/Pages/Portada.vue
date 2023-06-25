2bb3d9f7f1764fed747a724c15928ec621e28797
msoler75 <msoler75@yahoo.es>
2023-06-25 13:51:30 -0400

    SEO

---
+++ resources/js/Pages/Portada.vue
@@ -0,0 +1,19 @@
+<template>
+    <Sections>
+        <Section :style="{height: `calc(100% - 45)`}">
+            xxx
+        </Section>
+        <Section>
+            yyy
+        </Section>
+        <Section>
+            zzz
+        </Section>
+    </Sections>
+</template>
+
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+defineOptions({ layout: AppLayout })
+</script>
