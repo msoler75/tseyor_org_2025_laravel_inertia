d7beb16f97e065598df1f5b6ea98fd81943652f6
msoler75 <msoler75@yahoo.es>
2023-07-29 22:05:33 -0400

    content auto detect format

--- resources/js/Components/Content.vue
+++ resources/js/Components/Content.vue
@@ -1,7 +1,6 @@
 <template>
     <Prose ref="container">
-        {{ images }}
-        <Markdown v-if="isMarkdown" :source="content" />
+        <Markdown v-if="isMarkdown" :source="content" :html="true" :linkify="true" />
         <div v-else v-html="content" />
     </Prose>
 </template>
@@ -17,7 +16,7 @@ const props = defineProps({
     },
 });
 
-const isMarkdown = computed(() => true)
+const isMarkdown = computed(() => detectFormat(props.content).format == 'Markdown')
 const container = ref(null)
 const images = ref([])
 
@@ -25,6 +24,12 @@ onMounted(() => {
     nextTick(() => {
         console.log('container:', container.value.$el)
         const imgElements = container.value.$el.querySelectorAll('img');
+
+        // añadimos la clase especial para contenedor de imagenes
+        for(const img of imgElements)
+            img.parentNode.className='images-wrapper'
+
+        // guardamos el array de imagenes del contenido
         images.value = Array.from(imgElements)
         for (const index in images.value)
             images.value[index].addEventListener('click', () => handlePreview(index))
@@ -33,7 +38,37 @@ onMounted(() => {
 
 function handlePreview(index) {
     console.log('clicked ', index, images.value[index])
-    v3ImgPreviewFn({ images:images.value.map(img=>img.src), index })
+    v3ImgPreviewFn({ images: images.value.map(img => img.src), index })
+}
+
+
+function detectFormat(text) {
+    // Contamos la cantidad de etiquetas HTML
+    const htmlTagsCount = (text.match(/<\/?[a-z][a-z0-9]*\b[^>]*>/gi) || []).length;
+
+    // Contamos la cantidad de marcadores Markdown
+    const markdownMarkersCount = (text.match(/[*#_>\[\]`!-]|\!\[|\]\(/g) || []).length;
+
+    console.log({ htmlTagsCount, markdownMarkersCount })
+
+    // Usamos una expresión regular para detectar si hay algún patrón de HTML en el texto
+    const htmlPattern = /<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/i;
+    const containsHTMLPattern = htmlPattern.test(text);
+
+    // Calculamos la probabilidad de que sea Markdown o HTML
+    const totalMarkers = markdownMarkersCount + htmlTagsCount;
+    const markdownProbability = markdownMarkersCount / (totalMarkers + 1);
+    const htmlProbability = htmlTagsCount / (totalMarkers + 1);
+
+    // Establecemos un umbral de probabilidad para determinar el formato
+    const threshold = 0.6;
+    if (markdownProbability >= threshold && markdownProbability > htmlProbability) {
+        return { format: "Markdown", probability: markdownProbability };
+    } else if (htmlProbability >= threshold && htmlProbability > markdownProbability && containsHTMLPattern) {
+        return { format: "HTML", probability: htmlProbability };
+    } else {
+        return { format: "Ambiguous", probability: 0.5 };
+    }
 }
 
 </script>
@@ -41,6 +76,13 @@ function handlePreview(index) {
 
 <style scoped>
 :deep(img) {
-    @apply max-w-full mx-auto mb-3 block;
+    @apply max-w-full mx-auto mb-3;
+}
+/* amplia la imagen en el ancho */
+@media (min-width: 1154px) {
+    :deep(p.images-wrapper) {
+        width: 150%;
+        margin-left: -25%;
+    }
 }
 </style>
