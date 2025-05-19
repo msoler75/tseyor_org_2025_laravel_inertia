1ee831da0dfd4f3fa90862d721d0932a9ea84f4e
Marcel Soler Gallart <msoler75@yahoo.es>
2025-05-19 22:44:49 +0200

    ui

--- resources/js/Components/Avatar.vue
+++ resources/js/Components/Avatar.vue
@@ -1,12 +1,10 @@
 <template>
 
     <component :is="link ? User : 'div'" v-if="errorImage || !image || image.match(/^https:\/\/ui-avatars.com\/api\/\?name=/)"
-        :user="user" class="font-normal">
-        <div class="avatar avatar-placeholder">
+        :user="user" class="justify-center !flex font-normal avatar avatar-placeholder">
             <div class="ring-neutral ring-offset-base-100 ring ring-offset-2 bg-base-200 rounded-full"
             :class="imageClass">
                 <span class="text-base-content" :class="textClass">{{ initials(name) }}</span>
-            </div>
         </div>
     </component>
 
@@ -27,7 +25,7 @@ import { initials } from '@/composables/textutils.js'
 const props = defineProps({
     user: { type: Object, required: true },
     link: { type: Boolean, default: true },
-    imageClass: { type: String, required: false },
+    imageClass: { type: String, required: false, default: 'w-24 h-24' },
     textClass: { type: String, required: false },
     popupCard: { type: Boolean, default: true },
     lazy: {type: Boolean, default: false}
--- resources/js/Components/Users.vue
+++ resources/js/Components/Users.vue
@@ -1,16 +1,16 @@
 <template>
     <div v-if="compact" class="avatar-group -space-x-6">
-        <Avatar v-for="user of users" :key="user.id" :user="user" imageClass="w-14 h-14" />
+        <Avatar v-for="user of users" :key="user.id" :user="user" imageClass="w-14 h-14 flex-shrink-0" />
         <div v-if="count>users.length" class="avatar avatar-placeholder">
-            <div class="bg-neutral/50 text-neutral-content w-14 shrink-0 rounded-full">
+            <div class="bg-neutral/50 text-neutral-content shrink-0 rounded-full">
                 <span class="text-2xl">+{{count - users.length}}</span>
             </div>
         </div>
     </div>
     <GridFill v-else col-width="2.9rem" class="gap-1">
-        <Avatar v-for="user of users" :key="user.id" :user="user" imageClass="w-14 h-14 ring-neutral hover:ring-4 hover:ring-secondary " />
+        <Avatar v-for="user of users" :key="user.id" :user="user" imageClass="w-14 h-14 ring-neutral hover:ring-4 hover:ring-secondary flex-shrink-0" />
         <div v-if="count>users.length" class="avatar avatar-placeholder">
-            <div class="bg-neutral/50 text-neutral-content w-14 shrink-0 rounded-full">
+            <div class="bg-neutral/50 text-neutral-content shrink-0 rounded-full">
                 <span class="text-2xl">+{{count - users.length}}</span>
             </div>
         </div>
